a0f36243fd861ed5831c6ecca96d52dcad413352
nagarajansara <rnagarajan.ramalingam@gmail.com>
2015-11-24 21:21:45 +0530

    Cooment

--- src/main/java/travel/com/controller/MyHttpSessionListener.java
+++ src/main/java/travel/com/controller/MyHttpSessionListener.java
@@ -11,13 +11,13 @@ public class MyHttpSessionListener implements HttpSessionListener
 	public void sessionDestroyed(HttpSessionEvent event)
 	{
 		// Do here the job.
-		System.out.println("Seesion Destroy");
+		//System.out.println("Seesion Destroy");
 	}
 
 	public void sessionCreated(HttpSessionEvent arg0)
 	{
 		// TODO Auto-generated method stub
-		System.out.println("Seesion Created");
+		/* System.out.println("Seesion Created"); */
 	}
 
 }
