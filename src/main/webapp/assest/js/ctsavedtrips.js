86af1f2489bed1a975e726be231700ae704c645f
nagarajansara <rnagarajan.ramalingam@gmail.com>
2016-03-08 17:26:00 +0530

    C

---
+++ src/main/webapp/assest/js/ctsavedtrips.js
@@ -0,0 +1,23 @@
+function ctSetSavedListingPagination() {
+    $(".ctListingPaginationDiv").css({
+	display : "block"
+    });
+    numEntries = numEntries ? numEntries : 50;
+    entryPerPage = entryPerPage ? entryPerPage : 10;
+    visiblePageNo = visiblePageNo ? visiblePageNo : 5;
+    startPage = startPage ? startPage : 1;
+    if (numEntries && entryPerPage) {
+	var config = {
+	    "parentSelector" : "#pagination-demo",
+	    "totalPages" : numEntries,
+	    "visiblePages" : visiblePageNo,
+	    "entryPerPage" : entryPerPage,
+	    "startPage" : startPage,
+	    "cbk" : function(event, pageNo) {
+
+	    },
+	    "HREF_URL" : hrefURL
+	}, renderPagination = new RenderPagination(config);
+	renderPagination.render();
+    }
+}
